4a11df173e1f64b85295320b5bc869c2fd6df022
s2277816 <v.mutruc@student.utwente.nl>
2020-04-24 22:19:27 +0200

    first

---
+++ bridge/resources/test/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using GTANetworkAPI;
+
+namespace test
+{
+    public class Main : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void ResourceStart()
+        {
+            NAPI.Util.ConsoleOutput("Hello World2!");
+        }
+    }
+}
