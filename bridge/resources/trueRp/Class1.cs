5a171dd69d021e5dd17587e2df66e33440940743
s2277816 <v.mutruc@student.utwente.nl>
2020-04-28 01:40:21 +0200

    SQLite partialy implemented

---
+++ bridge/resources/trueRp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GTANetworkAPI;
+using System.Data.SQLite;
+
+namespace trueRp
+{
+    public class Class1 : Script
+    {
+       public Class1()
+        {
+            string conString = "DataSource=test.db";
+
+            using var con = new SQLiteConnection(conString);
+
+            con.Open();
+            Console.WriteLine("Hello Success!");
+        }
+    }
+}
